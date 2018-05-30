e14af95ed3b0d7a015e0daf362457eeef292bf96
Tim Manalo <tmanalo113@gmail.com>
2018-05-30 13:41:05 -0700

    undo

--- ui.R
+++ ui.R
@@ -2,8 +2,6 @@
 library(shiny)
 library(plotly)
 library(shinythemes)
-source("movie_summary.R")
-
 shinyUI(navbarPage(
   theme = shinytheme("slate"),
   "Movie Report",
@@ -27,16 +25,6 @@ shinyUI(navbarPage(
           label = "Choose Movie",
           choices = movie_choices
         ),
-<<<<<<< HEAD
-      htmlOutput("img1"),
-        tags$br(),
-      HTML("<strong>Story Line: </strong>"),
-        htmlOutput("overview")
-
-      ),
-
-      mainPanel(column(8, align="center"),
-=======
         htmlOutput("img1"),
         tags$br(),
         HTML("<strong>Story Line: </strong>"),
@@ -44,29 +32,11 @@ shinyUI(navbarPage(
       ),
 
       mainPanel(
->>>>>>> 00c1945fdc33ad9e9c3c5561135db0cc87c84843
         plotlyOutput("bar")
       )
     )
   ),
   tabPanel(
-<<<<<<< HEAD
-    "Overview",
-    titlePanel("Film Description"),
-    sidebarLayout(
-      sidebarPanel(
-        selectInput(
-          "film",
-          label = "Choose Film",
-          choices = movie_choices
-        )
-      ),
-      mainPanel(
-        htmlOutput("poster"),
-        h4("Overview"),
-        h5(textOutput("description")),
-        h5(textOutput("genre"))
-=======
     "Profit Per Minute",
     # Create sidebar layout
     sidebarLayout(
@@ -85,34 +55,10 @@ shinyUI(navbarPage(
 
       mainPanel(
         plotlyOutput("scatter")
->>>>>>> 00c1945fdc33ad9e9c3c5561135db0cc87c84843
       )
     ),
   tabPanel(
-    "Last Graph Here"
+    "Last graph"
   )
   )
-<<<<<<< HEAD
-  # tabPanel(
-  #   "Overview",
-  #   titlePanel("Film Description"),
-  #   sidebarLayout(
-  #     sidebarPanel(
-  #       selectInput(
-  #         "film",
-  #         label = "Choose Film",
-  #         choices = movie_choices
-  #       )
-  #     ),
-  #     # htmlOutput("img1"),
-  #     #   tags$br()
-  #     mainPanel(
-  #       htmlOutput("img1"),
-  #       tags$br()
-  #     )
-  #   )
-  # )
-))
-=======
 )
->>>>>>> 00c1945fdc33ad9e9c3c5561135db0cc87c84843
