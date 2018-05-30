cff7cd44c7fe56c9463062f55b1a2b67e1666547
Jack Weinstein <jackweinstein98@gmail.com>
2018-05-29 17:58:26 -0700

    working progress

--- api_keys.R
+++ api_keys.R
@@ -1,2 +1,4 @@
-nyt_movies_key <- 'f90bbf91f1dd49239a566f594af2b31f'
-movie_db_key <- '14e91502a9d163c67468ed9b24d40282'
+nyt_movies_key <- "f90bbf91f1dd49239a566f594af2b31f"
+movie_db_key <- "14e91502a9d163c67468ed9b24d40282"
+fandango_key <- "hhxvtpuurwvek36ehde2p3xh"
+secret <- "w3C7HAfVwp"
--- nyt_wrangling.R
+++ nyt_wrangling.R
@@ -7,9 +7,18 @@ nyt_base_uri <- "http://api.nytimes.com/svc/movies/v2"
 nyt_resource <- "/reviews/all.json"
 nyt_uri_full <- paste0(nyt_base_uri, nyt_resource)
 
-query_params <- list(key = nyt_movies_key)
-GET(nyt_uri_full, querystring = query_params)
+query_params <- list("api-key" = nyt_movies_key)
+body <- GET(nyt_uri_full, query = query_params)
+body_content <- fromJSON(content(body, "text"))
+flattened <- flatten(body_content$results)
 # query_params <- list(key = civic_key, address = selected_address)
 # response <- GET(full_uri, query = query_params)
 # response_content <- content(response, "text")
 # parsed_data <- fromJSON(response_content)
+
+fandango_uri <- "http://api.fandango.com/v1?op="
+nyt_resource <- ""
+# operation
+# parameter list
+# apikey and sig
+"<operation>&<parameter list>&apikey=<apikey>&sig=<sig>"
