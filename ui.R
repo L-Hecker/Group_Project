00c1945fdc33ad9e9c3c5561135db0cc87c84843
Tim Manalo <tmanalo113@gmail.com>
2018-05-30 13:09:41 -0700

    edit ui

--- ui.R
+++ ui.R
@@ -9,11 +9,11 @@ shinyUI(navbarPage(
     "Overview",
     h1("Our Project"),
     p("This app utilizies TMDb's API to view data on specific movies and answer questions about a movie's success or possible shortcomings. For this project, we pull in the most recent data available on the date this app is accessed and showcases 20 of the most popular new/upcoming films. We provide information on a movie's finances as well as its popularity, giving the audience a chance to see how much money goes into making a film and how much a film needs to make to be profitable."),
-    h2("TMDb's API"),
+    h1("TMDb's API"),
     p("The data we worked with came from", tags$a(href="https://www.themoviedb.org/documentation/api?language=en", "The Movie Database."), "TMDb is a community built movie and TV database that allowed us to view a movie's budget, revenue, popularity, runtime, etc. Though our project is focused on movies in english, TMDb's database allows anyone to access international movies from", tags$b("all around the world.")),
-    h3("Overall Movie Profits"),
+    h1("Overall Movie Profits"),
     p("Here you may view finances on the most recent/upcoming films. You may view the film's budget, revenue, and profit. Also provided is the movie poster and a short overview of the film's plot"),
-    h4("Profits Per Minute"),
+    h1("Profits Per Minute"),
     p("This section allows you to visualize how much profit a film has made for every minute of the film's runtime. Because we use the most recent data, some film's may not have an updated revenue report or have yet to break the profit margins.")
     ),
   tabPanel(
@@ -56,6 +56,9 @@ shinyUI(navbarPage(
       mainPanel(
         plotlyOutput("scatter")
       )
-    )
+    ),
+  tabPanel(
+    "Last Graph Here"
+  )
   )
 )
